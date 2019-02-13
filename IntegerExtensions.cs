22e1a7d7308886e11611f608d10d4a49c36b3b26
sttr19 <stastroshko@gmail.com>
2019-02-13 15:36:31 +0300

    Update IntegerExtensions.cs

--- IntegerExtensions.cs
+++ IntegerExtensions.cs
@@ -6,8 +6,12 @@ namespace PadawansTask3
     {
         public static int Gcd(int a, int b)
         {
-            // put your code here
-            throw new NotImplementedException();
+            while(a!=b)
+            {
+                if (a > b) a -= b;
+                else b -= a;
+            }
+            return a;
         }
     }
 }
